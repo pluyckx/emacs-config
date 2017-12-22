06be2b3f50130584f72772585bdcd4baa51b270b
Philip Luyckx <philip.luyckx@gmail.com>
2017-12-22 17:07:00 +0100

    Add magit
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (json-mode js2-mode company-tern treemacs company-web))))
+    (magit json-mode js2-mode company-tern treemacs company-web))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
