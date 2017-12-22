cdc0dc434cd096b773a79b441d917a77b795b577
Philip Luyckx <philip.luyckx@gmail.com>
2017-12-22 15:31:39 +0100

    Add treemacs
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- init.el
+++ init.el
@@ -13,10 +13,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company-web))))
+ '(package-selected-packages (quote (treemacs))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(treemacs)
