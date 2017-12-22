844012e6c36dae8177af20ebec0622d1e010089e
Philip Luyckx <philip.luyckx@gmail.com>
2017-12-22 15:32:35 +0100

    Add configuration for htm(l) with company mode
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- init.el
+++ init.el
@@ -13,12 +13,41 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (treemacs))))
+ '(package-selected-packages (quote (treemacs company-web))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Configure web-mode
+(require 'web-mode)
+
+;; Configure Company
+(require 'company)                                   ; load company mode
+
+(setq company-minimum-prefix-length 0)            ; WARNING, probably you will get perfomance issue if min len is 0!
+(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-tooltip-align-annotations 't)          ; align annotations to the right tooltip border
+(setq company-idle-delay .1)                         ; decrease delay before autocompletion popup shows
+(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+
+;; Company-web
+(require 'company-web-html)                          ; load company mode html backend
+;; and/or
+(require 'company-web-jade)                          ; load company mode jade backend
+(require 'company-web-slim)                          ; load company mode slim backend
+
+;; you may key bind, for example for web-mode:
+(add-hook 'web-mode-hook (lambda()
+			   (set (make-local-variable 'company-backends) '(company-web-html))
+			   (company-mode t)))
+
+(define-key web-mode-map (kbd "C-;") 'company-complete)
+
+
+;; Set default modes based on extensions
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 (treemacs)
