9541781a2153cdc235defd2b26d71bb7ee6ae86c
Philip Luyckx <philip.luyckx@gmail.com>
2017-12-22 16:16:41 +0100

    Add javascript complete when in web mode inside script tag
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- init.el
+++ init.el
@@ -43,7 +43,7 @@
 
 ;; you may key bind, for example for web-mode:
 (add-hook 'web-mode-hook (lambda()
-			   (set (make-local-variable 'company-backends) '(company-web-html))
+			   (set (make-local-variable 'company-backends) '(company-tern company-web-html))
 			   (company-mode t)))
 
 ;; Company-tern
@@ -53,6 +53,16 @@
 			   (tern-mode)
 			   (company-mode)))
 
+;; Enable JavaScript completion between <script>...</script> etc.
+(advice-add 'company-tern :before
+	    #'(lambda (&rest _)
+		(if (equal major-mode 'web-mode)
+		    (let ((web-mode-cur-language
+			   (web-mode-language-at-pos)))
+		      (if (or (string= web-mode-cur-language "javascript")
+			      (string= web-mode-cur-language "jsx"))
+			  (unless tern-mode (tern-mode))
+			(if tern-mode (tern-mode -1)))))))
 
 ;; Define auto complete for company
 (define-key company-mode-map (kbd "C-;") 'company-complete)
